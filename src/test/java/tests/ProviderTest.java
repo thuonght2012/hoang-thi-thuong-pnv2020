46297dc76929070f0c1d053f8f4a83dbd98711f7
thuonghoang2012 <thuong.hoangpn@gmail.com>
2020-01-08 10:39:21 +0700

    commit full codes1

--- src/test/java/tests/ProviderTest.java
+++ src/test/java/tests/ProviderTest.java
@@ -1,14 +1,12 @@
 package tests;
 
 import org.openqa.selenium.support.PageFactory;
-import pages.CheckDepositPage;
 import pages.LoginPage;
 import runner.TestRunner;
 
 public class ProviderTest {
 
     private static LoginPage loginPage;
-    private static CheckDepositPage checkDepositPage;
 
 
     public static LoginPage getLoginPage() {
@@ -16,11 +14,5 @@ public class ProviderTest {
             loginPage = PageFactory.initElements(TestRunner.driver, LoginPage.class);
         }
         return loginPage;
-    }
-    public static CheckDepositPage getCheckDepositPage() {
-        if (checkDepositPage == null) {
-            checkDepositPage = PageFactory.initElements(TestRunner.driver, CheckDepositPage.class);
-        }
-        return checkDepositPage;
     }
 }
